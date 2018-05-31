2e3162a107748f291e424b8357e5a43bb6f87a18
Ruben <santipab.stephane@gmail.com>
2018-05-31 12:36:29 -0500

    initial commit

---
+++ Assets/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    public float rate;
+    public GameObject prefab;
+    public float range;
+    float TheTimer=0;
+	void Update () {
+        TheTimer += Time.deltaTime;
+        if (TheTimer >= rate)
+        {
+            TheTimer = 0;
+            GameObject.Instantiate(prefab, new Vector3(Random.Range(-range, range), 0, Random.Range(-range, range)),Quaternion.identity);
+        }
+	}
+}
